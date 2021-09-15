af8ffab7563f3c0a34ae01791a561e2132e32d88
Michaelsky0913 <40595864+Michaelsky0913@users.noreply.github.com>
2021-09-15 12:09:10 +0800

    modify args

--- filter_binlog2sql_result.py
+++ filter_binlog2sql_result.py
@@ -21,7 +21,7 @@ def set_log_format():
 
     # set logger color
     log_colors_config = {
-        'DEBUG': 'bold_puple',
+        'DEBUG': 'bold_purple',
         'INFO': 'bold_green',
         'WARNING': 'bold_yellow',
         'ERROR': 'bold_red',
@@ -61,13 +61,13 @@ def parse_args():
                                      formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument('--help', dest='help', action='store_true', help='help information', default=False)
 
-    parser.add_argument('-s', '--source-file', dest='sql_file', type=str,
+    parser.add_argument('-f', '--file', dest='sql_file', type=str,
                         help='sql file you want to filter', default='')
     parser.add_argument('-p', '--pk', dest='primary_key', type=str,
                         help='choose a column to be primary key', default='`id`')
     parser.add_argument('-k', '--kcl', dest='keep_col_list', type=str, nargs='*',
                         help='choose multi column that you want to keep, these column will not be filter', default=[])
-    parser.add_argument('-t', '--out-file', dest='out_file', type=str,
+    parser.add_argument('-o', '--out-file', dest='out_file', type=str,
                         help='file that save the result', default='')
     return parser
 
@@ -106,9 +106,6 @@ def read_file(filename, is_yield=False):
 
 
 def fix_json_col(col_list):
-    """
-    col_list must split with space, not comma
-    """
     # 左括号数量 与 右括号数量
     json_mark_left_cnt = 0
     json_mark_right_cnt = 0
@@ -205,7 +202,7 @@ def main(args):
     sql_file = args.sql_file
     keep_col_list = args.keep_col_list
     primary_key = args.primary_key
-    out_file = args.out_file if args.out_file else 'new_' + sql_file
+    out_file = args.out_file
     logger.warning('This function only filter update statement')
 
     sql_file_len = get_file_lines(sql_file)
@@ -216,19 +213,27 @@ def main(args):
     finished_info = info_format + 'finished'
     info_format += '[Filtered line count: {cnt} / {sql_file_len}]'
 
+    f = open(out_file, 'w', encoding='utf8') if out_file else ''
+    line = ''
     try:
-        with open(out_file, 'w', encoding='utf8') as f:
-            for line in read_file(sql_file, is_yield=True):
-                new_sql = filter_update(line, primary_key=primary_key, keep_col_list=keep_col_list)
+        for line in read_file(sql_file, is_yield=True):
+            new_sql = filter_update(line, primary_key=primary_key, keep_col_list=keep_col_list)
+            if f:
                 f.write(new_sql + '\n')
-                cnt += 1
-                if (cnt % 10000) == 0:
-                    logger.info(info_format.format(cnt=cnt, sql_file_len=sql_file_len))
-            logger.info(info_format.format(cnt=cnt, sql_file_len=sql_file_len))
-            logger.info(finished_info)
+            else:
+                print(new_sql)
+            cnt += 1
+            if (cnt % 10000) == 0:
+                logger.info(info_format.format(cnt=cnt, sql_file_len=sql_file_len))
+        logger.info(info_format.format(cnt=cnt, sql_file_len=sql_file_len))
+        logger.info(finished_info)
+        if out_file:
             logger.warning("The result saved in %s" % out_file)
     except Exception as e:
         logger.error('Detect error in line: [' + str(line) + '], err_msg is: ' + str(e))
+    finally:
+        if f:
+            f.close()
 
 
 if __name__ == "__main__":
